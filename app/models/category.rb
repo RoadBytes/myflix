59d8a056ad10df8b352055da67fea52a5b8cb4a0
Jason Data <jason.data@roadbytes.me>
2015-06-11 17:42:19 +0800

    completed recent_videos method before looking at solution

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :videos, -> { order(:title) }
   validates :name, presence: :true
+
+  def recent_videos
+    self.videos.sort_by{|video| video.created_at }.reverse[0, 6]
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,40 @@ require 'spec_helper'
 describe Category do
   it { should have_many(:videos) }
   it { should validate_presence_of(:name) }
+
+  describe "method recent_videos" do
+    before :each do
+       @drama   = create(:category, name: "Drama")
+       @first   = create(:video, title: "First",   created_at: 1.day.ago, category: @drama)
+       @second  = create(:video, title: "Second",  created_at: 2.day.ago, category: @drama)
+       @third   = create(:video, title: "Third",   created_at: 3.day.ago, category: @drama)
+       @fourth  = create(:video, title: "Fourth",  created_at: 4.day.ago, category: @drama)
+       @fifth   = create(:video, title: "Fifth",   created_at: 5.day.ago, category: @drama)
+       @sixth   = create(:video, title: "Sixth",   created_at: 6.day.ago, category: @drama)
+       @sevneth = create(:video, title: "Sevneth", created_at: 7.day.ago, category: @drama)
+ 
+       @comedy         = create(:category, name: "Comedy")
+       @comedy_first   = create(:video, title: "Comedy First",  created_at: 1.day.ago, category: @comedy)
+       @comedy_second  = create(:video, title: "Comedy Second", created_at: 2.day.ago, category: @comedy)
+       @comedy_third   = create(:video, title: "Comedy Third",  created_at: 3.day.ago, category: @comedy)
+    end
+
+    it "will return an empty array if there are no videos" do
+      adventure = create(:category)
+      expect(adventure.recent_videos).to eq([])
+    end
+
+    it "will return all videos if there are less than six videos in the category" do
+      expect(@comedy.recent_videos).to include(@comedy_first, @comedy_second, @comedy_third)
+    end
+    
+    it "will return six videos in a category" do
+      expect(@drama.recent_videos.size).to eq(6)
+    end
+
+    it "will return an array of six videos ordered from recent to earlier" do
+      expect(@drama.recent_videos).to eq([@first, @second, @third, @fourth, @fifth, @sixth])
+    end
+  end
 end
+
