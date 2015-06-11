5913ab52254bbcf397b9ff39c8c6985abad4861e
Jason Data <jason.data@roadbytes.me>
2015-06-11 14:49:08 +0800

    added changes to form in re to TA Brandon feedback

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -8,4 +8,8 @@ describe Video do
   it { should belong_to(:category)}
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  context "search by title"
+
+  end
 end
