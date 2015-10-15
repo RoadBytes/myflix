0ea512f5e1003b9d6010741dcf6e0c2ce0f7ac05
Jason Data <jason.data@roadbytes.me>
2015-10-15 17:35:39 +0800

    followed rating feature from queue_list from solution

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -6,17 +6,31 @@ class QueueItem < ActiveRecord::Base
 
   delegate :category, to: :video
 
-  def user_rating
-    review = Review.where(user_id: user.id, video_id: video.id).first
+  def rating
     review.rating unless review.nil?
+  end
+
+  def rating=(new_rating)
+    unless review.nil?
+      review.update_column(:rating, new_rating)
+    else
+      review = Review.new(user_id: user.id, video_id: video.id, rating: new_rating)
+      review.save(validate: false)
+    end
   end
 
   def self.update_queue_items(queue_items, authenticated_user)
     ActiveRecord::Base.transaction do
       queue_items.each do |queue_item_data| 
         queue_item = QueueItem.find_by id: queue_item_data["id"]
-        queue_item.update_attributes!(position: queue_item_data["position"]) if queue_item.user == authenticated_user
+        queue_item.update_attributes!(position: queue_item_data["position"], rating: queue_item_data["rating"]) if queue_item.user == authenticated_user
       end
     end
+  end
+
+  private
+
+  def review
+    @review ||= Review.where(user_id: user.id, video_id: video.id).first
   end
 end
