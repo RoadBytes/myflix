4b7ef87f1719be34cacb599deb7deac76a37189d
Jason Data <jason.data@roadbytes.me>
2015-12-09 14:03:29 +0930

    added admin video create features with s3 and carrierwave-aws

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,9 @@ class AdminsController < ApplicationController
   before_filter :ensure_admin
 
   def ensure_admin
-    flash[:danger] = 'Access Denied'
-    redirect_to root_path unless current_user.admin?
+    unless current_user && current_user.admin?
+      flash[:danger] = 'Access Denied'
+      redirect_to root_path 
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124092203) do
+ActiveRecord::Schema.define(version: 20151208114851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,12 +67,12 @@ ActiveRecord::Schema.define(version: 20151124092203) do
   create_table "videos", force: true do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "large_image_url"
-    t.string   "small_image_url"
     t.float    "rating"
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "large_cover"
+    t.string   "small_cover"
   end
 
 end
